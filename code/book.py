eb9314c0ea5296b2544d97cfbe307762529f922b
GraceBruketa <161533297+GraceBruketa@users.noreply.github.com>
2024-04-21 13:43:29 -0600

    Update book.py
    updated genre dictionary to match constant formatting specifications (upppercase)

--- code/book.py
+++ code/book.py
@@ -1,6 +1,6 @@
 class Book:
         #genre info displayed in a dict, keys as genre ID, and values as name of genre
-    genre_name= {
+    GENRE_NAME_DICT= {
         0:"Romance",
         1:"Mystery",
         2:"Science Fiction",
@@ -31,10 +31,13 @@ class Book:
         return self.__author
     
     def get_genre_name(self):
-        return self.__genre
+        return Book.GENRE_NAME_DICT.get(self.__genre)
+    
+    def get_available(self):
+        return self.__availability
     
     def get_availability(self):
-        if self.__availability == True:
+        while self.__availability == True:
             return "Available"
         else:
             return "Borrowed"
@@ -48,6 +51,9 @@ class Book:
         
     def set_author(self, author):
         self.__author = author
+    
+    def set_genre(self, genre_id):
+       self.__genre = genre_id
         
     def borrow_it(self):
         self.__availability = False
@@ -59,10 +65,8 @@ class Book:
         genre_name = self.get_genre_name()
         availability_bool = self.get_availability()
         print(f'ISBN: {self.__isbn}\nTitle: {self.__title}\nAuthor: {self.__author}\nGenre: {genre_name}\nAvailability: {availability_bool}')
-
-    def set_genre(self, genre):
-        self.__genre = genre
-   
+    
+    
     def print_all(books):
         print(f'{"ISBN":<16} {"Title":<30} {"Author":<21} {"Genre":<21} {"Availability":<11}')
         for book in books:
@@ -72,4 +76,5 @@ class Book:
         genre_name = self.get_genre_name()
         availability_bool = self.get_availability()
         results =  f"{self.__isbn:<14} {self.__title:<25} {self.__author:<25} {genre_name:<20} {availability_bool:<10}"
+   
         return results
